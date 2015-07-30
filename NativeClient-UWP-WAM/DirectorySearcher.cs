b59d33b256ae7bbf0ea83ca6ebf1b8a5bc4f92ae
Vittorio Bertocci <vibro@libero.it>
2015-07-30 10:26:17 -0700

    first load

---
+++ NativeClient-UWP-WAM/DirectorySearcher.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NativeClient_UWP_WAM
+{
+    public static class DirectorySearcher
+    {
+        const string graphResourceUri = "https://graph.windows.net";
+        public static string graphApiVersion = "1.5";
+
+        public static async Task<List<UserSearchResult>> SearchByAlias(string alias, string accessToken, string tenantId)
+        {
+            JObject jResult = null;
+            List<UserSearchResult> results = new List<UserSearchResult>();
+
+            try
+            {
+                string graphRequest = String.Format(CultureInfo.InvariantCulture, "{0}/{1}/users?api-version={2}&$filter=mailNickname eq '{3}'", graphResourceUri, tenantId, graphApiVersion, alias);
+                HttpClient client = new HttpClient();
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, graphRequest);
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+                HttpResponseMessage response = await client.SendAsync(request);
+
+                string content = await response.Content.ReadAsStringAsync();
+                jResult = JObject.Parse(content);
+            }
+            catch (Exception ee)
+            {
+                results.Add(new UserSearchResult { error = ee.Message });
+                return results;
+            }
+
+            if (jResult["odata.error"] != null)
+            {
+                results.Add(new UserSearchResult { error = (string)jResult["odata.error"]["message"]["value"] });
+                return results;
+            }
+            if (jResult["value"] == null)
+            {
+                results.Add(new UserSearchResult { error = "Unknown Error." });
+                return results;
+            }
+            foreach (JObject result in jResult["value"])
+            {
+                results.Add(new UserSearchResult
+                {
+                    displayName = (string)result["displayName"],
+                    givenName = (string)result["givenName"],
+                    surname = (string)result["surname"],
+                    userPrincipalName = (string)result["userPrincipalName"],
+                    telephoneNumber = (string)result["telephoneNumber"] == null ? "Not Listed." : (string)result["telephoneNumber"]
+                });
+            }
+
+            return results;
+        }
+    }
+
+    public class UserSearchResult
+    {
+        public string displayName { get; set; }
+        public string userPrincipalName { get; set; }
+        public string givenName { get; set; }
+        public string surname { get; set; }
+        public string telephoneNumber { get; set; }
+        public string error { get; set; }
+    }
+}
---
+++ NativeClient-UWP-WAM/MainPage.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Security.Authentication.Web;
+using Windows.Security.Authentication.Web.Core;
+using Windows.Security.Credentials;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace NativeClient_UWP_WAM
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        //
+        // The Client ID is used by the application to uniquely identify itself to Azure AD.
+        // The Tenant is the name of the Azure AD tenant in which this application is registered.
+        // The AAD Instance is the instance of Azure, for example public Azure or Azure China.
+        // The Authority is the sign-in URL of the tenant.
+
+        const string clientId = "a9b55b7d-66af-4de9-9ee7-c7b04106bdef";
+
+        // const string clientId = "1847e0b6-739e-4786-81f7-d73a9bc8ba44";
+        // const string tenant = "common";
+        // const string tenant = "vibro21hotmail.onmicrosoft.com";
+        const string tenant = "developertenant.onmicrosoft.com";
+        //  const string tenant = "microsoft.onmicrosoft.com";
+        //   const string tenant = "cloudidentity.net";
+        // const string authority = "https://login.microsoftonline.com/" + tenant;
+        const string authority = "organizations";
+
+        // To authenticate to the directory Graph, the client needs to know its App ID URI.
+        const string resource = "https://graph.windows.net";
+
+        // For this preview, Windows10 universal apps require redirect URI in the format below
+        string URI = string.Format("ms-appx-web://Microsoft.AAD.BrokerPlugIn/{0}", WebAuthenticationBroker.GetCurrentApplicationCallbackUri().Host.ToUpper());
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            List<UserSearchResult> results = new List<UserSearchResult>();
+            results.Add(new UserSearchResult());
+            SearchResults.ItemsSource = results;
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            string a = null;
+        }
+
+
+        //private async Task<string> GetTokenViaADAL()
+        //{
+        //    AuthenticationContext ac = new AuthenticationContext(authority);
+        //    AuthenticationResult ar = await ac.AcquireTokenAsync(resource, clientId, new Uri(URI));
+        //    return ar.AccessToken;
+        //}
+
+        private async void Search(object sender, RoutedEventArgs e)
+        {
+
+            string accessToken = string.Empty;
+
+            WebAccountProvider wap =
+               await WebAuthenticationCoreManager.FindAccountProviderAsync("https://login.microsoft.com", authority);
+
+            //   WebTokenRequest wtr = new WebTokenRequest(wap, string.Empty, clientId, WebTokenRequestPromptType.ForceAuthentication);
+            WebTokenRequest wtr = new WebTokenRequest(wap, string.Empty, clientId);
+
+            wtr.Properties.Add("resource", resource);
+
+            WebTokenRequestResult wtrr = await WebAuthenticationCoreManager.RequestTokenAsync(wtr);
+            if (wtrr.ResponseStatus == WebTokenRequestStatus.Success)
+            {
+                accessToken = wtrr.ResponseData[0].Token;
+                var account = wtrr.ResponseData[0].WebAccount;
+                var properties = wtrr.ResponseData[0].Properties;
+            }
+
+            // TEST TEST
+            // accessToken = await GetTokenViaADAL();
+
+            SearchResults.ItemsSource = await DirectorySearcher.SearchByAlias(SearchTermText.Text, accessToken, tenant);
+        }
+    }
+}
